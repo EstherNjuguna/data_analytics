3b79bdf6debd0c101bd2b166ca059f5436bf09e9
+ <+>
2023-09-25 17:11:21 +0300

    added r script

--- meriskill_diabetes/README.md
+++ meriskill_diabetes/README.md
@@ -1,1 +1,3 @@
-Data analytics on diabetes
+BPs cannot be zero so we replaced all the zero Bp with the mean BPs.
+Age was placed into categories.
+BMI was also replaced all ) with the mean
--- meriskill_diabetes/diabetes.R
+++ meriskill_diabetes/diabetes.R
@@ -3,14 +3,13 @@
  library(ggplot2)
  library(dplyr)
  library(tidyr)
- install.packages("corrplot")
  library(corrplot)
  diabetes <- read_csv("~/data_analytics/meriskill_diabetes/diabetes.csv")
  head(diabetes)
  # number of columns and rows
  dim(diabetes)
  str(diabetes)
- #summarise the data
+ # summarise the data
  summary(diabetes)
 # find NAs
  diabetes %>% map(~sum(is.na(.)))
@@ -31,6 +30,7 @@
    )
 min(diabetes$Age)
 max(diabetes$Age)
+# C
  diabetes <- diabetes %>%
    mutate(
      age_cat = case_when(
@@ -43,9 +43,21 @@ max(diabetes$Age)
        TRUE ~ 'Unknown'  # Default category for other cases
      )
    )
- diabetes
+ # Replace 0 Bp with the mean
+ diabetes <- diabetes %>%
+    mutate(BloodPressure = ifelse(BloodPressure == 0, mean(BloodPressure, na.rm = TRUE), BloodPressure))
+ ## Replace 0 BMI with mean
+ diabetes <- diabetes %>%
+   mutate(BMI = replace(BMI, BMI == 0, mean(BMI, na.rm = TRUE)))
+ # Replace 0 glucose levels
+ diabetes <- diabetes %>%
+   mutate(Glucose = replace(Glucose, Glucose == 0, mean(Glucose, na.rm = TRUE)))
+ # Replace 0 skin Thickness
+ diabetes <- diabetes %>%
+   mutate(SkinThickness = replace(SkinThickness, SkinThickness == 0, mean(SkinThickness, na.rm = TRUE)))
+ summary(diabetes)
 
- 
+ ## categorize age vs the outcome
  ggplot(diabetes, aes(x = age_cat, fill = Outcome)) +
    geom_bar() +
    labs(
@@ -55,8 +67,9 @@ max(diabetes$Age)
    ) +
    scale_fill_brewer(palette = "Set3") +
    theme_minimal()
+ 
  cor( diabetes$BMI,diabetes$Insulin)
-  diabetes_numeric <- diabetes[, c("Age", "BMI", "BloodPressure", "Glucose", "Insulin", "SkinThickness")]
+  diabetes_numeric <- diabetes[, c("Pregnancies", "Age","DiabetesPedigreeFunction",  "Outcome","BMI", "BloodPressure", "Glucose", "Insulin", "SkinThickness")]
  
  # Calculate the correlation matrix
  correlation_matrix <- cor(diabetes_numeric)
@@ -68,9 +81,10 @@ max(diabetes$Age)
    type = "upper",    # Show only the upper triangle of the matrix
    tl.col = "black",  # Label color
    tl.srt = 45,       # Label rotation angle
-   tl.pos = "lt",     # Label position
+   tl.pos = "lt",   # Label position
    diag = FALSE,      # Exclude the diagonal
    addCoef.col = "black",  # Color of correlation coefficients
    tl.cex = 0.7       # Label text size
  )
+ 
  
